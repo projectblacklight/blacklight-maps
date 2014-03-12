7f3a055a06e74f5844bfadaa82065f1dbf811552
Jack Reed <pjreed@stanford.edu>
2014-03-12 15:59:51 -0700

    refactored js to function as a jquery plugin
     requires geojson_docs, and allows for additional options

--- app/assets/javascripts/blacklight-maps/blacklight-maps-browse.js
+++ app/assets/javascripts/blacklight-maps/blacklight-maps-browse.js
@@ -1,128 +1,137 @@
-var map, sidebar;
+;(function( $ ) {
 
-Blacklight.onLoad(function() {
+  $.fn.blacklight_leaflet_map = function(geojson_docs, arg_opts) {
+    var map, sidebar, markers, geoJsonLayer;
 
-  // Stop doing stuff if the map div isn't there
-  if ($("#blacklight-map").length === 0){
-    return;
-  }
-  
-  // Get the configuration options from the data-attributes
-  $.extend(Blacklight.mapOptions, $("#blacklight-map").data());
+    // Configure default options and those passed via the constructor options
+    var options = $.extend({
+      datatype : "placename_coordinates",
+      tileurl : 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      mapattribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      sidebar: 'blacklight-map-sidebar'
+    }, arg_opts );
 
-  map = L.map('blacklight-map').setView([0,0], 2);
-  L.tileLayer(Blacklight.mapOptions.tileurl, {
-    attribution: Blacklight.mapOptions.mapattribution,
-    maxZoom: Blacklight.mapOptions.maxzoom
-  }).addTo(map);
+    // Extend options from data-attributes
+    $.extend(options, this.data());
 
-  // Sets up leaflet-sidebar
-  sidebar = L.control.sidebar('blacklight-map-sidebar', {
-    position: 'right',
-    autoPan: false
-  });
+    // Display the map
+    this.each(function() {
 
-  // Adds leaflet-sidebar control to map (object)
-  map.addControl(sidebar);
+      // Setup Leaflet map
+      map = L.map(this.id).setView([0,0], 2);
+      L.tileLayer(options.tileurl, {
+        attribution: options.mapattribution,
+        maxZoom: options.maxzoom
+      }).addTo(map);
 
-  // Create a marker cluster object and set options
-  markers = new L.MarkerClusterGroup({
-    showCoverageOnHover: false,
-    spiderfyOnMaxZoom: false,
-    singleMarkerMode: true,
-    animateAddingMarkers: true
-  });
-
-  geoJsonLayer = L.geoJson(geojson_docs, {
-    onEachFeature: function(feature, layer){
-      layer.defaultOptions.title = feature.properties.placename;
-      layer.on('click', function(e){
-        hideSidebar();
-        var placenames = {};
-        placenames[feature.properties.placename] = [feature.properties.html];
-        offsetMap(e);
-        $('#blacklight-map-sidebar').html(buildList(placenames));
-        sidebar.show();
+      // Initialize sidebar
+      sidebar = L.control.sidebar(options.sidebar, {
+        position: 'right',
+        autoPan: false
       });
-    }
-  });
 
-  // Add GeoJSON layer to marker cluster object
-  markers.addLayer(geoJsonLayer);
+      // Adds leaflet-sidebar control to map
+      map.addControl(sidebar);
 
-  // Add marker cluster object to map
-  map.addLayer(markers);
+      // Create a marker cluster object and set options
+      markers = new L.MarkerClusterGroup({
+        showCoverageOnHover: false,
+        spiderfyOnMaxZoom: false,
+        singleMarkerMode: true,
+        animateAddingMarkers: true
+      });
 
-  // Listeners for marker cluster clicks
-  markers.on('clusterclick', function(e){
-      
-    //hide sidebar if it is visible
-    hideSidebar();
+      geoJsonLayer = L.geoJson(geojson_docs, {
+        onEachFeature: function(feature, layer){
+          layer.defaultOptions.title = feature.properties.placename;
+          layer.on('click', function(e){
+            hideSidebar();
+            var placenames = {};
+            placenames[feature.properties.placename] = [feature.properties.html];
+            offsetMap(e);
+            $('#' + options.sidebar).html(buildList(placenames));
+            sidebar.show();
+          });
+        }
+      });
 
-    //if map is at the lowest zoom level
-    if (map.getZoom() === Blacklight.mapOptions.maxzoom){
+      // Add GeoJSON layer to marker cluster object
+      markers.addLayer(geoJsonLayer);
 
-      var placenames = generatePlacenamesObject(e.layer.getAllChildMarkers());
-      
+      // Add marker cluster object to map
+      map.addLayer(markers);
 
-      offsetMap(e);
+      // Listeners for marker cluster clicks
+      markers.on('clusterclick', function(e){
 
-      //Update sidebar div with new html
-      $('#blacklight-map-sidebar').html(buildList(placenames));
+        //hide sidebar if it is visible
+        hideSidebar();
 
-      //Show the sidebar!
-      sidebar.show();
-    }
-  });
+        //if map is at the lowest zoom level
+        if (map.getZoom() === options.maxzoom){
 
-  //Add click listener to map
-  map.on('click drag', hideSidebar);
+          var placenames = generatePlacenamesObject(e.layer.getAllChildMarkers());
 
-});
 
-Blacklight.mapOptions = {
-  tileurl : 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  mapattribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-};
+          offsetMap(e);
 
-function hideSidebar(){
-  if (sidebar.isVisible()){
-    sidebar.hide();
-  }
-}
+          //Update sidebar div with new html
+          $('#' + options.sidebar).html(buildList(placenames));
 
-function buildList(placenames){
-  var html = "";
-  $.each(placenames, function(i,val){
-    html += "<h2>" + i + "</h2>";
-    html += "<ul class='sidebar-list'>";
-    $.each(val, function(j, val2){
-      html += val2;
+          //Show the sidebar!
+          sidebar.show();
+        }
+      });
+
+      //Add click listener to map
+      map.on('click drag', hideSidebar);
+
     });
-    html += "</ul>";
-  });
-  return html;
-}
 
-// Generates placenames object 
-function generatePlacenamesObject(markers){
-  var placenames = {};
-  $.each(markers, function(i,val){
-    if (!(val.feature.properties.placename in placenames)){
-      placenames[val.feature.properties.placename] = [];
+    // Hides sidebar if it is visible
+    function hideSidebar(){
+      if (sidebar.isVisible()){
+        sidebar.hide();
+      }
     }
-    placenames[val.feature.properties.placename].push(val.feature.properties.html);
-  });
-  return placenames;
-}
 
-// Move the map so that it centers the clicked cluster TODO account for various size screens
-function offsetMap(e){
-  mapWidth = $('#blacklight-map').width();
-  mapHeight = $('#blacklight-map').height();
-  if (!e.latlng.equals(map.getCenter())){
-    map.panBy([(e.originalEvent.layerX - (mapWidth/4)), (e.originalEvent.layerY - (mapHeight/2))]);
-  }else{
-    map.panBy([(mapWidth/4), 0]);
-  }
-}
+    // Build the list
+    function buildList(placenames){
+      var html = "";
+      $.each(placenames, function(i,val){
+        html += "<h2>" + i + "</h2>";
+        html += "<ul class='sidebar-list'>";
+        $.each(val, function(j, val2){
+          html += val2;
+        });
+        html += "</ul>";
+      });
+      return html;
+    }
+
+    // Generates placenames object
+    function generatePlacenamesObject(markers){
+      var placenames = {};
+      $.each(markers, function(i,val){
+        if (!(val.feature.properties.placename in placenames)){
+          placenames[val.feature.properties.placename] = [];
+        }
+        placenames[val.feature.properties.placename].push(val.feature.properties.html);
+      });
+      return placenames;
+    }
+
+    // Move the map so that it centers the clicked cluster TODO account for various size screens
+    function offsetMap(e){
+      var mapWidth = $('#blacklight-map').width();
+      var mapHeight = $('#blacklight-map').height();
+      if (!e.latlng.equals(map.getCenter())){
+        map.panBy([(e.originalEvent.layerX - (mapWidth/4)), (e.originalEvent.layerY - (mapHeight/2))]);
+      }else{
+        map.panBy([(mapWidth/4), 0]);
+      }
+    }
+
+  };
+
+}( jQuery ));
