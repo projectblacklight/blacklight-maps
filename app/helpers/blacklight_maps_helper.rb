bb87836a0b9b5f6a3753be7543dd6e91e28be911
Justin Coyne <justin@curationexperts.com>
2014-03-19 14:58:33 -0500

    Refactor #show_map_div to match Rails tag rendering conventions. Fixes #6

--- app/helpers/blacklight_maps_helper.rb
+++ app/helpers/blacklight_maps_helper.rb
@@ -1,13 +1,19 @@
 # Helper methods used for Blacklight Maps
 module BlacklightMapsHelper
-  def show_map_div
-    data_attributes = {
-        maxzoom: blacklight_config.view.maps.maxzoom,
-        tileurl: blacklight_config.view.maps.tileurl
+  # @param [String] id the html id
+  # @param [Hash] tag_options options to put on the tag
+  def blacklight_map_tag id, tag_options = {}, &block
+    default_data = {
+      maxzoom: blacklight_config.view.maps.maxzoom,
+      tileurl: blacklight_config.view.maps.tileurl
+    }
 
-      }
-
-    tag('div', id: 'blacklight-map', data: data_attributes)
+    options = {id: id, data: default_data}.deep_merge(tag_options)
+    if block_given?
+      content_tag(:div, options, &block)
+    else
+      tag(:div, options)
+    end
   end
 
   def serialize_geojson
