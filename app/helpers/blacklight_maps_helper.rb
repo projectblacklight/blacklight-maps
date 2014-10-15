1fd0ccdc7430fa38768a24ef260d2db538744834
Eben English <eenglish@bpl.org>
2014-10-15 16:03:40 -0400

    initial work on map for catalog#show view

--- app/helpers/blacklight_maps_helper.rb
+++ app/helpers/blacklight_maps_helper.rb
@@ -1,13 +1,14 @@
-# Helper methods used for Blacklight Maps
 module BlacklightMapsHelper
+  # TODO move methods to BlacklightMaps::BlacklightMapsHelperBehavior # figure out why this doesn't work!
+
   # @param [String] id the html id
   # @param [Hash] tag_options options to put on the tag
   def blacklight_map_tag id, tag_options = {}, &block
     default_data = {
-      maxzoom: blacklight_config.view.maps.maxzoom,
-      tileurl: blacklight_config.view.maps.tileurl,
-      type: blacklight_config.view.maps.type,
-      mapattribution: blacklight_config.view.maps.mapattribution
+        maxzoom: blacklight_config.view.maps.maxzoom,
+        tileurl: blacklight_config.view.maps.tileurl,
+        type: blacklight_config.view.maps.type,
+        mapattribution: blacklight_config.view.maps.mapattribution
     }
 
     options = {id: id, data: default_data}.deep_merge(tag_options)
@@ -18,9 +19,18 @@ module BlacklightMapsHelper
     end
   end
 
+  def link_to_geo_facet(field_value, field, displayvalue = nil)
+    new_params = {}
+    new_params[:action] = 'index'
+    new_params[:controller] = 'catalog'
+    new_params[:f] = {field => [field_value]}
+    link_to(displayvalue.presence || field_value, new_params)
+  end
+
   def serialize_geojson
     export = BlacklightMaps::GeojsonExport.new(controller,
                                                @response.docs)
     export.to_geojson
   end
+
 end
