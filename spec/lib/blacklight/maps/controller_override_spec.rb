7a99c6db6d9de0eb498e7d01be69a614f20d672f
Eben English <eenglish@bpl.org>
2015-01-30 10:27:51 -0500

    fixes for failing tests with ruby-1.9.3

--- spec/lib/blacklight/maps/controller_override_spec.rb
+++ spec/lib/blacklight/maps/controller_override_spec.rb
@@ -11,22 +11,22 @@ describe BlacklightMaps::ControllerOverride do
     @fake_controller.extend(BlacklightMaps::ControllerOverride)
   end
 
-  let(:solr_params) { OpenStruct.new }
-  let(:req_params) { OpenStruct.new }
+  let(:solr_params) { Hash.new }
+  let(:req_params) { Hash.new }
 
   describe "add_spatial_search_to_solr" do
 
     it "should return a coordinate point spatial search if coordinates are given" do
-      req_params.coordinates = "35.86166,104.195397"
-      req_params.spatial_search_type = "point"
-      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params).fq.first).to include('geofilt')
-      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params).pt).to eq(req_params.coordinates)
+      req_params[:coordinates] = "35.86166,104.195397"
+      req_params[:spatial_search_type] = "point"
+      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params)[:fq].first).to include('geofilt')
+      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params)[:pt]).to eq(req_params[:coordinates])
     end
 
     it "should return a bbox spatial search if a bbox is given" do
-      req_params.coordinates = "[6.7535159,68.162386 TO 35.5044752,97.395555]"
-      req_params.spatial_search_type = "bbox"
-      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params).fq.first).to include(CatalogController.blacklight_config.view.maps.coordinates_field)
+      req_params[:coordinates] = "[6.7535159,68.162386 TO 35.5044752,97.395555]"
+      req_params[:spatial_search_type] = "bbox"
+      expect(@fake_controller.add_spatial_search_to_solr(solr_params, req_params)[:fq].first).to include(CatalogController.blacklight_config.view.maps.coordinates_field)
     end
 
   end
